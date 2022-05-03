43d905654d25f40f2fc655e5d54de4efeee5c863
emilywnwong <69983880+emilywnwong@users.noreply.github.com>
2022-05-04 01:00:45 +0200

    implement functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
 import './App.css';
 
+import React, { useState } from 'react';
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const questions = [
+		{
+			questionText: 'Which country is this person from?',
+			answerOptions: [
+				{ answerText: 'Malaysia', isCorrect: false },
+				{ answerText: 'China', isCorrect: false },
+				{ answerText: 'Japan', isCorrect: true },
+				{ answerText: 'Thailand', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Which country is this person from?',
+			answerOptions: [
+				{ answerText: 'Singapore', isCorrect: false },
+				{ answerText: 'Vietnam', isCorrect: true },
+				{ answerText: 'Philippines', isCorrect: false },
+				{ answerText: 'South Korea', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Which country is this person from?',
+			answerOptions: [
+				{ answerText: 'North Korea', isCorrect: true },
+				{ answerText: 'Hong Kong', isCorrect: false },
+				{ answerText: 'China', isCorrect: false },
+				{ answerText: 'South Korea', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Which country is this person from?',
+			answerOptions: [
+				{ answerText: 'Malaysia', isCorrect: false },
+				{ answerText: 'China', isCorrect: false },
+				{ answerText: 'Japan', isCorrect: false },
+				{ answerText: 'Thailand', isCorrect: true },
+			],
+		},
+	];
+
+	const [currentQuestion, setCurrentQuestion] = useState(0);
+	const [showScore, setShowScore] = useState(false);
+	const [score, setScore] = useState(0);
+
+	const handleAnswerOptionClick = (isCorrect) => {
+		if (isCorrect) {
+			setScore(score + 1);
+		}
+
+		const nextQuestion = currentQuestion + 1;
+		if (nextQuestion < questions.length) {
+			setCurrentQuestion(nextQuestion);
+		} else {
+			setShowScore(true);
+		}
+	};
+	return (
+		<div className='app'>
+			{showScore ? (
+				<div className='score-section'>
+					You scored {score} out of {questions.length}
+				</div>
+			) : (
+				<>
+					<div className='question-section'>
+						<div className='question-count'>
+							<span>Question {currentQuestion + 1}</span>/{questions.length}
+						</div>
+						<div className='question-text'>{questions[currentQuestion].questionText}</div>
+					</div>
+					<div className='answer-section'>
+						{questions[currentQuestion].answerOptions.map((answerOption) => (
+							<button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+						))}
+					</div>
+				</>
+			)}
+		</div>
+	);
 }
 
 export default App;
